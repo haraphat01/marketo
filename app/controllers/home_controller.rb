9bc83907415c77561fddf9d5f15370fc64179734
haraphat01 <haraphat@gmail.com>
2023-01-15 22:13:02 +0100

    Added devise for authentication

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    def index
+    end
+    def create 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
